a7d11afe7c6a29af62e8c3731c6db6a5097367fe
caamomilaa <c.velazquezdamico@gmail.com>
2025-04-16 07:46:27 +0200

    update

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+// Instrucciones importantes:
+// El número de intentos debe poder cambiarse desde una variable.
+// El número de celdas para las letras dependerá de la longitud de la palabra que salga.
+// Recordad que existen los elementos del DOM, los children, los bucles anidados, los replace...
+// Antes de escribir, PENSAD. Un sólo problema cada vez.
+// Por último, las apariencias ENGAÑAN.
+
+const gameboardElement = document.getElementById('gameboard');
+const solutionButtonElement = document.getElementById('solution-button');
+
+const allWords = [
+	'zara',
+	'pelo',
+	'humo',
+	'chasqueador',
+	'zombie',
+	'atropello',
+	'ticket',
+	'infectado',
+	'hambre',
+	'accidente',
+	'vitrina',
+	'apocalipsis',
+	'supervivencia',
+	'sangre',
+	'hongo',
+	'cordyceps'
+];
+const numberOfTries = 5;
+
+let wordSelected = '';
+
+const getRandomWord = () => {
+	const randomPosition = Math.floor(Math.random() * allWords.length);
+	const randomWord = allWords[randomPosition];
+
+	wordSelected = randomWord;
+};
+getRandomWord();
+
+const paintCells = () => {
+	const cell = document.createElement('div');
+	cell.classList.add('cell');
+	// gameboardElement.prepend(cell);
+
+	const totalLetters = wordSelected.length;
+
+	for (let i = 0; i < totalLetters; i++) {}
+
+	// for (const letter of totalLetters) {
+	// 	const totalCells = letter;
+	// 	gameboardElement.prepend(totalCells);
+	// }
+};
+paintCells();
