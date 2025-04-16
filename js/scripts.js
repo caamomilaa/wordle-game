4cb04b45486e2ba1bcbad9ce15f330b5911df461
caamomilaa <163582100+caamomilaa@users.noreply.github.com>
2025-04-16 11:57:05 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -6,53 +6,109 @@
 // Por último, las apariencias ENGAÑAN.
 
 const gameboardElement = document.getElementById('gameboard');
-const solutionButtonElement = document.getElementById('solution-button');
+const formElement = document.getElementById('form');
+const solutionInputElement = document.getElementById('solution-input');
+const messageElement = document.getElementById('message');
 
-const allWords = [
-	'zara',
-	'pelo',
-	'humo',
-	'chasqueador',
-	'zombie',
-	'atropello',
-	'ticket',
-	'infectado',
-	'hambre',
-	'accidente',
-	'vitrina',
-	'apocalipsis',
-	'supervivencia',
-	'sangre',
-	'hongo',
-	'cordyceps'
-];
+const allWords = ['zara'];
+
+// const allWords = [
+//   'zara',
+//   'pelo',
+//   'humo',
+//   'chasqueador',
+//   'zombie',
+//   'atropello',
+//   'ticket',
+//   'infectado',
+//   'hambre',
+//   'accidente',
+//   'vitrina',
+//   'apocalipsis',
+//   'supervivencia',
+//   'sangre',
+//   'hongo',
+//   'cordyceps'
+// ];
 const numberOfTries = 5;
+const alphabet = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
 
 let wordSelected = '';
+let currentRow = 0;
 
 const getRandomWord = () => {
-	const randomPosition = Math.floor(Math.random() * allWords.length);
-	const randomWord = allWords[randomPosition];
+  const randomPosition = Math.floor(Math.random() * allWords.length);
+  const randomWord = allWords[randomPosition];
 
-	wordSelected = randomWord;
+  return randomWord;
 };
-getRandomWord();
 
 const paintCells = () => {
-	// const cell = document.createElement('div');
-	// cell.classList.add('cell');
-	// gameboardElement.prepend(cell);
-	const totalLetters = wordSelected.length;
+  wordSelected = getRandomWord();
+  const totalLetters = wordSelected.length;
 
-	for (let i = 0; i < totalLetters; i++) {
-		const cell = document.createElement('div');
-		cell.classList.add('cell');
-		gameboardElement.prepend(cell);
-	}
-
-	// for (const letter of totalLetters) {
-	// 	const totalCells = letter;
-	// 	gameboardElement.prepend(totalCells);
-	// }
+  for (let i = 0; i < numberOfTries; i++) {
+    const cellsContainer = document.createElement('div');
+    cellsContainer.classList.add('cells-container');
+    for (let j = 0; j < totalLetters; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cellsContainer.append(cell);
+    }
+    gameboardElement.append(cellsContainer);
+  }
 };
 paintCells();
+
+const putWordInCell = () => {
+  let inputLength = solutionInputElement.value.length;
+
+  for (let i = 0; i < inputLength; i++) {
+    gameboardElement.children[currentRow].children[i].textContent =
+      solutionInputElement.value[i];
+  }
+  //Llamo al hijo de gameboard de currentRow, para ir a la fila que quiero. Luego, quiero ir al hijo del Row, que es i, porque estoy recorriendo cada letra dentro del bucle.
+
+  currentRow++;
+};
+
+const getClassColor = () => {
+  //hay que recorrer cada letra del y ver si el valor de alphabet(includes) es igual. Hay que reocrrer cada letra del input
+  //dvidir letras del input > buclaction
+  //dividir letras del la palabra seleccionada
+  //comparar
+  //cojo zara > z a r a > cojo la z y comparo
+  //PRIMERA LETRA DEL ARRAY
+  //   const firstLetterInput = wordSelected.charAt(0);
+  //   console.log(firstLetterInput);
+
+  //   console.log(allWords[0].charAt(0));
+
+  //   if (firstLetterInput === allWords[0].charAt(0)) {
+  //     console.log(true);
+  //   }
+
+  const wordLength = wordSelected.length;
+
+  for (let i = 0; i < wordLength; i++) {
+    if (allWords[0].charAt(i) === solutionInputElement.value.charAt(i)) {
+      console.log('ta bien');
+    }
+  }
+};
+
+const putWordInRow = event => {
+  event.preventDefault();
+  getClassColor();
+  const wordLength = wordSelected.length;
+  const inputLength = solutionInputElement.value.length;
+
+  if (wordLength === inputLength) {
+    putWordInCell();
+    messageElement.textContent = '';
+  } else {
+    messageElement.textContent = `The word doesn't have ${wordLength} characters`;
+  }
+  event.target.reset();
+};
+formElement.addEventListener('submit', putWordInRow);
